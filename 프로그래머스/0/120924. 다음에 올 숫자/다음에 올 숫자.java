2fedcb72e15852adebd23a1857bbb3483e078a4b
yap! <sonsy090909@gmail.com>
2025-02-17 21:46:13 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -2,16 +2,22 @@ class Solution {
     public int solution(int[] common) {
         int answer = 0;
         int diff = common[1] - common[0];
+        int diff1 = common[2] - common[1];
 
         boolean isDiff = true;
 
-        for(int i = 0; i < common.length - 1; i++){
-            if(common[i+1] - common[i] != diff){
-                isDiff = false;
-            }
-        }
-        if(isDiff){
-           answer = common[common.length - 1] + diff;
+        // for(int i = 0; i < common.length - 1; i++){
+        //     if(common[i+1] - common[i] != diff){
+        //         isDiff = false;
+        //     }
+        // }
+        // if(isDiff){
+        //    answer = common[common.length - 1] + diff;
+        // }else{
+        //     answer = common[common.length - 1] * common[1]/common[0];
+        // }
+        if(diff == diff1){
+            answer = common[common.length - 1] + diff;
         }else{
             answer = common[common.length - 1] * common[1]/common[0];
         }
