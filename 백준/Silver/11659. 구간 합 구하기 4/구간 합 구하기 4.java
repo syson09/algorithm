794108fd1ca2c8cd79ba2d5e5ee16cee2a56ca66
yap! <sonsy090909@gmail.com>
2025-02-20 20:17:42 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 584 ms, Memory: 53140 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,26 +1,34 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
         int N = Integer.parseInt(stringTokenizer.nextToken());
         int M = Integer.parseInt(stringTokenizer.nextToken());
-        long[]S = new long[N + 1];
+
+        int[] S = new int[N + 1];  // int 배열 사용하여 메모리 최적화
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        for(int i = 1; i <= N; i++){
-            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+        for (int i = 1; i <= N; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
         }
-        for(int k = 0; k<M; k++){
+        StringBuilder sb = new StringBuilder();
+        for (int k = 0; k < M; k++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             int i = Integer.parseInt(stringTokenizer.nextToken());
             int j = Integer.parseInt(stringTokenizer.nextToken());
-            System.out.println(S[j] - S[i -1]);
+            sb.append(S[j] - S[i - 1]).append("\n");
         }
+        bufferedWriter.write(sb.toString());  // 한 번에 출력
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
     }
 }
