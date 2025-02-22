8a90129c31637460ad790500f7598bd579e3202a
yap! <sonsy090909@gmail.com>
2025-02-22 22:30:17 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16752 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -2,24 +2,29 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
+        int[] a = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int[] array = new int[N];
-        for(int i = 0; i<N; i++){
-            array[i] = Integer.parseInt(st.nextToken());
-        }
-        int cnt = 0;
         for(int i = 0; i < N; i++){
-            for(int j = i + 1; j < N; j++){
-                if(array[i] + array[j] == M) {
-                    cnt++;
-                }
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a); // 오름차순으로 정렬
+        int start = 0, end = N-1, cnt = 0;
+        while(start < end){
+            if(a[start] + a[end] == M){
+                cnt++;
+                start ++;
+                end --;
+            }else if(a[start] + a[end] < M ){
+                start++;
+            }else{
+                end --;
             }
         }
         System.out.println(cnt);
